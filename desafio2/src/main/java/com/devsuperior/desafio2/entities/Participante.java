7059cfcc4755814680d84421fcf0b940be47a25c
bredaneves <bredaneves@gmail.com>
2025-08-21 23:54:04 -0300

    Modelo de dominio e ORM - desafio 2

---
+++ desafio2/src/main/java/com/devsuperior/desafio2/entities/Participante.java
@@ -0,0 +1,75 @@
+package com.devsuperior.desafio2.entities;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Participante {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+	private String nome;
+	private String email;
+	
+	@ManyToMany(mappedBy = "participantes")
+    private Set<Atividade> atividades = new HashSet<>();
+	
+	public Participante() {		
+	}
+
+	public Participante(Integer id, String nome, String email) {		
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}	
+
+	public Set<Atividade> getAtividades() {
+		return atividades;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Participante other = (Participante) obj;
+		return Objects.equals(id, other.id);
+	}	
+
+}
