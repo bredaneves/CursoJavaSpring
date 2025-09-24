5e40b6003c2bcfd1d687a57b3ae3f80714828cd9
bredaneves <bredaneves@gmail.com>
2025-09-23 21:11:12 -0300

    Desafio 4 - primeira tentativa

---
+++ desafio4/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -0,0 +1,51 @@
+package com.devsuperior.dsmeta.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
+import com.devsuperior.dsmeta.services.SaleService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+
+	@Autowired
+	private SaleService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<SaleMinDTO> findById(@PathVariable Long id) {
+		SaleMinDTO dto = service.findById(id);
+		return ResponseEntity.ok(dto);
+	}
+
+	@GetMapping(value = "/report")
+    public ResponseEntity<Page<SaleMinDTO>> getReport(
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+            @RequestParam(value = "name", defaultValue = "") String name,
+            Pageable pageable) {
+
+        Page<SaleMinDTO> result = service.getReport(minDate, maxDate, name, pageable);
+        return ResponseEntity.ok(result);
+    }
+
+	@GetMapping(value = "/summary")
+    public ResponseEntity<List<SaleSummaryDTO>> getSummary(
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+
+        List<SaleSummaryDTO> result = service.getSummary(minDate, maxDate);
+        return ResponseEntity.ok(result);
+    }
+}
---
+++ desafio4/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,49 @@
+package com.devsuperior.dsmeta.repositories;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
+import com.devsuperior.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+	
+	@Query(
+      nativeQuery = true,
+      value = 
+          "SELECT s.id, s.date, s.amount, se.name AS sellerName "
+        + "FROM tb_sales s "
+        + "INNER JOIN tb_sellers se ON s.seller_id = se.id "
+        + "WHERE s.date BETWEEN :minDate AND :maxDate "
+        + "AND UPPER(se.name) LIKE CONCAT('%', UPPER(:name), '%') "
+        + "ORDER BY s.date DESC "      
+    )
+    Page<SaleMinDTO> searchReport(
+        @Param("minDate") LocalDate minDate,
+        @Param("maxDate") LocalDate maxDate,
+        @Param("name") String name,
+        Pageable pageable
+    );
+	
+	@Query(
+      nativeQuery = true,
+      value = 
+          "SELECT se.name AS sellerName, SUM(s.amount) AS total "
+        + "FROM tb_sales s "
+        + "INNER JOIN tb_sellers se ON s.seller_id = se.id "
+        + "WHERE s.date BETWEEN :minDate AND :maxDate "
+        + "GROUP BY se.name "
+        + "ORDER BY se.name "      
+    )
+    List<SaleSummaryDTO> searchSummary(
+        @Param("minDate") LocalDate minDate,
+        @Param("maxDate") LocalDate maxDate
+    );
+}
