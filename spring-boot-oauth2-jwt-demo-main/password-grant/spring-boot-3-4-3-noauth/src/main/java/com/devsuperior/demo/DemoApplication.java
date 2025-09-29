1b8481659c68f821078fcdb85689f94c7f60a00e
bredaneves <bredaneves@gmail.com>
2025-09-29 16:17:05 -0300

    Cap05 - BCrypt password

--- spring-boot-oauth2-jwt-demo-main/password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ spring-boot-oauth2-jwt-demo-main/password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,30 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private PasswordEncoder passwordEnconder; 
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("ENCODE = " + passwordEnconder.encode("123456"));
+		
+		boolean result = passwordEnconder.matches("123456", "$2a$10$czLaAFuRv37kRyK/gxIAx.yuR2/I.u7RE5NTSPm.fd64AgM0vqNm.");
+		
+		System.out.println("RESULTADO = " + result );
+	}
+	
+	
 
 }
