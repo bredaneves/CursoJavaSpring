9f67d7aa88e50a7024c614ace7ac7aa5581f48ba
bredaneves <bredaneves@gmail.com>
2025-10-03 22:09:19 -0300

    Aula - correcao main

--- spring-boot-oauth2-jwt-demo-main/password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ spring-boot-oauth2-jwt-demo-main/password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,12 +1,13 @@
 package com.devsuperior.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication /*implements CommandLineRunner*/ {
+public class DemoApplication implements CommandLineRunner {
 	
 	@Autowired
 	private PasswordEncoder passwordEnconder; 
