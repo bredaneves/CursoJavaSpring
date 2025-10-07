578b4c4964d8d7dd359225daeadb659c5f6fc5bc
bredaneves <bredaneves@gmail.com>
2025-10-06 21:53:48 -0300

    Desafio5 - password encoder

--- desafio5/src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
+++ desafio5/src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
@@ -1,13 +1,28 @@
 package com.devsuperior.dscommerce;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DscommerceApplication {
+public class DscommerceApplication implements CommandLineRunner {
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder; 
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscommerceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("ENCODE = " + passwordEncoder.encode("123456"));
+		
+		boolean result = passwordEncoder.matches("123456", "$2a$10$gktAI1ZIbccGxDRpAahPH.lPT5tXwd0hH.a.FiDAFj/qyUMlo2Ppa");
+		
+		System.out.println("RESULTADO = " + result);
 	}
 
 }
